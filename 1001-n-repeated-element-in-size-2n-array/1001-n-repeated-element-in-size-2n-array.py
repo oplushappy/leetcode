b4c7af2b606a180a4ed22ea0f08b45adb346282b
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-01-02 12:32:57 +0800

    1001-N-REPEATED-ELEMENT-IN-SIZE-2N-ARRAY Stats: Time: 0 ms (100%), Space: 18.2 MB (100%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -3,7 +3,8 @@ class Solution:
         for i in range(len(nums) - 2):
             if nums[i] == nums[i + 1] or nums[i] == nums[i + 2]:
                 return nums[i]
-            
+        
+        # nums[-2], nums[-1], ex: [1, 2, 3, 3]
         return nums[-1]
 
 
