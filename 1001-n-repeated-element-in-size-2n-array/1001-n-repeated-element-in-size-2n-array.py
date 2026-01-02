205ee1dc8597b85c74c8df1a9dd1fa7d23b14c41
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-01-02 12:23:48 +0800

    1001-N-REPEATED-ELEMENT-IN-SIZE-2N-ARRAY Stats: Time: 17 ms (15.26%), Space: 18.5 MB (95.44%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,19 +1,16 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        s = len(nums)
-        n = s // 2
-        map = defaultdict(list)
-        nums.sort()
-        for i in range(s - 1):
-            cnt = 1
-            while nums[i] == nums[i + 1]:
-                i += 1
-                cnt += 1
-                if i == s - 1:
-                    break
-            map[cnt].append(nums[i])
+        map = defaultdict(int)
+
+        for i in range(len(nums)):
+            map[nums[i]] += 1
         
-        return map[n][0]
+        n = len(nums) // 2
+        for key, value in map.items():
+            if value == n:
+                return key
+        
+
 
         
             
