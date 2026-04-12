7fdca35b09e32c7631aec41096e02088191e8c1d
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-04-12 21:49:29 +0800

    Time: 16 ms (67.61%), Space: 21 MB (8.1%) - LeetHub

---
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxRepOpt1(self, text: str) -> int:
+        A = [[c, len(list(g))] for c, g in itertools.groupby(text)]
+        count = collections.Counter(text)
+        res = max(min(k + 1, count[c]) for c, k in A)
+        for i in range(1, len(A) - 1):
+            if A[i - 1][0] == A[i + 1][0] and A[i][1] == 1:
+                res = max(res, min(A[i - 1][1] + A[i + 1][1] + 1, count[A[i+1][0]]))
+        return res
