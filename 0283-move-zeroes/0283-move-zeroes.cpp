29f775586cc7b342e2e7ae3e65f4c90828d2c1a5
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-02 22:51:41 +0800

    Time: 0 ms (100%), Space: 23.7 MB (83.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        for(int l = 0, r = 1; l < n && r < n;r++) {
-            if(nums[l] && nums[r]) l++;
-            if(nums[l] && nums[r] == 0) l++;
-            if(nums[l] == 0 && nums[r]) {
+        int l = 0;
+        for(int r = 0; r < n; r++) {
+            if(nums[r]) {
                 int tmp = nums[l];
                 nums[l] = nums[r];
                 nums[r] = tmp;
