7b83f2a59395c7fceccb993fbaafe707fd5de626
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-04 15:07:37 +0800

    Time: 7 ms (91.59%), Space: 19.6 MB (26.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        map = defaultdict(int)
-        for char in s:
-            map[char] += 1
-
-        for char in t:
-            if char not in map : return False
-            if not map[char] : return False
-            map[char] -= 1
-        if len(s) != len(t) : return False
-        
-        return True
+        return Counter(s) == Counter(t)
