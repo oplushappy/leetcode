650d3f409e657dccabd98f534c09796b284ec628
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-05 22:57:11 +0800

    Time: 0 ms (100%), Space: 21.7 MB (86.97%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // int n = board.size(), m = board[0].size();
-        vector<unordered_set<int>> rows(9);
-        vector<unordered_set<int>> cols(9);
-        auto block = vector(3, vector<unordered_set<int>>(3));
+        vector<int> rows(9, 0), cols(9, 0), block(9, 0);
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(board[i][j] == '.') continue;
-                int num = board[i][j] - '0';
-                if(rows[i].count(num) || cols[j].count(num) || block[i/3][j/3].count(num)) {
+                int num = board[i][j] - '1';
+                int bit = 1 << num;
+                int b = (i / 3) * 3 + (j / 3);
+                if((rows[i] & bit) || (cols[j] & bit) || (block[b] & bit)) {
                     return false;
-                } else {
-                    rows[i].insert(num);
-                    cols[j].insert(num);
-                    block[i/3][j/3].insert(num);
                 }
+                rows[i] |= bit;
+                cols[j] |= bit;
+                block[b] |= bit;
+
             }
         }
         return true;
