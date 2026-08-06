ea9e33e5a766707e28e389f7ee61f41cfd30bf67
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-06 16:33:32 +0800

    Time: 0 ms (100%), Space: 15.1 MB (69.2%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-
-        while(l <= r) {
-            int m = (l + r) / 2;
-            
-            if(target == nums[m]) return m;
-
-            // left portion
-            if(nums[m] >= nums[l]) {
-                if(target > nums[m] || target < nums[l]) {
-                    l = m + 1;
-                } else {
-                    r = m - 1;
-                }
-            } else { // right portion
-                if(target < nums[m] || target > nums[r]) {
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }
-            }
+    template<typename T, typename M>
+    T get_first_match(T lo, T hi, M match) {
+        while(lo <= hi) {
+            T mid = lo + (hi - lo) / 2;
+            if(match(mid)) hi = mid - 1;
+            else lo = mid + 1;
         }
-        
-        return -1;
+        return lo;
+    };
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int rightest = nums[n - 1];
+        int spot = get_first_match(0, n - 1, [&](int idx) {
+            return nums[idx] <= rightest;
+        });
+        if(n == 1) return nums[0] == target ? 0 : -1;
+        int res1 = get_first_match(0, spot - 1, [&](int idx) {
+            return nums[idx] >= target;
+        });
+        int res2 = get_first_match(spot, n - 1, [&](int idx) {
+            return nums[idx] >= target;
+        });
+        if(res1 < n && nums[res1] == target) return res1;
+        else if(res2 < n && nums[res2] == target) return res2;
+        else return -1;
     }
 };
