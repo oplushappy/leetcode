d710792fb048f62070af0dd1db61b98ac5e0a288
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-06 11:39:58 +0800

    Time: 0 ms (100%), Space: 31.3 MB (82.4%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        while(l <= r) {
-            int m = l + ((r - l) /2);
-            if(nums[m] > target) {
-                r = m - 1;
-            } else if(nums[m] < target) {
-                l = m + 1;
-            } else {
-                return m;
-            }
+    template<typename T, typename M>
+    T get_first_match(T lo, T hi, M match) {
+        while(lo <= hi) {
+            T mid = lo + (hi - lo) / 2;
+            if(match(mid)) hi = mid - 1;
+            else lo = mid + 1;
         }
-        return -1;
+        return lo;
+    };
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int res = get_first_match(0, n, [&](int idx){
+            if(idx < n && idx >= 0) {
+                return nums[idx] >= target;
+            }
+            return false;
+        });
+        if(res < n && res >= 0) {
+            return nums[res] == target ? res : -1;
+        } else {
+            return -1;
+        }
+        // return res;
     }
 };
