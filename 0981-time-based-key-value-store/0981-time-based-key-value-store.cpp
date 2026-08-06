c79304dfed88f1db376c24abadcda4375f8b7fd5
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-07 01:18:37 +0800

    Time: 88 ms (9.93%), Space: 136.5 MB (94.45%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,16 +1,30 @@
 class TimeMap {
 public:
-    unordered_map<string, map<int, string>> m;
-    
-    TimeMap() {}
+    template<typename T, typename M>
+    T get_first_match(T lo, T hi, M match) {
+        while(lo <= hi) {
+            T mid = lo + (hi - lo) / 2;
+            if(match(mid)) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return lo;
+    };
+
+    unordered_map<string, vector<pair<int, string>>> timemap;
+    TimeMap() {
+    }
     
     void set(string key, string value, int timestamp) {
-        m[key].insert({timestamp, value});
+        timemap[key].emplace_back(timestamp, value);
     }
     
     string get(string key, int timestamp) {
-        auto it = m[key].upper_bound(timestamp);
-        return it == m[key].begin() ? "" : prev(it)->second;
+        int n = timemap[key].size();
+        int res = get_first_match(0, n - 1, [&](int idx) {
+            return timemap[key][idx].first > timestamp;
+        });
+        if((res - 1) >= 0) return timemap[key][res - 1].second;
+        else return "";
     }
 };
 
