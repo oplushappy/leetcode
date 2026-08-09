f570ca9ce86f94d00e6c70644d9d38ae749c034c
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-09 12:26:39 +0800

    Time: 3 ms (25.71%), Space: 16.8 MB (51.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,38 +5,36 @@ public:
     };
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
+        int fresh = 0;
 
         queue<tuple<int, int, int>> q; // x, y, minutes
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 2) {
                     q.emplace(i, j, 0);
+                } else if(grid[i][j] == 1) {
+                    fresh++;
                 }
             }
         }
+        if(fresh == 0) return 0;
+
         auto inside = [&](int x, int y) {
             return x >= 0 && x < m && y >= 0 && y < n;
         };
-        int res = 0;
         while(!q.empty()) {
             auto [x, y, minutes] = q.front();
             q.pop();
-            res = minutes;
             for(auto [dx, dy] : dirs) {
                 int ddx = x + dx, ddy = y + dy;
                 if(!inside(ddx, ddy)) continue;
                 if(grid[ddx][ddy] == 0 || grid[ddx][ddy] == 2) continue;
                 q.emplace(ddx, ddy, minutes + 1);
                 grid[ddx][ddy] = 2;
+                fresh--;
+                if(fresh == 0) return minutes + 1;
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
-        return res;
+        return -1;
     }
 };
