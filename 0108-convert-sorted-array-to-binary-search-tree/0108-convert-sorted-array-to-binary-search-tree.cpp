f6389a16a8ae8442e568cfd68f139a95d1b0af31
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-14 23:23:08 +0800

    Time: 3 ms (60.64%), Space: 23 MB (19.56%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -12,25 +12,14 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> visited(n, false);
-        auto build = [&](auto &self, int l, int r, TreeNode* cur) {
-            if(l < 0 || r < 0 || l >= n || r >= n) return;
-            if(l > r) return;
+        auto build = [&](auto &self, int l, int r) -> TreeNode* {
+            if(l > r) return nullptr;
             int mid = l + (r - l) / 2;
-            if(!(mid >= 0 && mid < n)) return;
-            if(visited[mid]) return;
-            visited[mid] = true;
             TreeNode* node = new TreeNode(nums[mid]);
-            if(nums[mid] > cur->val) cur->right = node;
-            else cur->left = node;
-            self(self, l, mid-1, node);
-            self(self, mid + 1, r, node);
+            node->left = self(self, l, mid - 1);
+            node->right = self(self, mid + 1, r);
+            return node;
         };
-        int m = nums.size()/ 2;
-        TreeNode* root = new TreeNode(nums[m]);
-        build(build, 0, m - 1, root);
-        build(build, m + 1, nums.size() - 1, root);
-        return root;
+        return build(build, 0, nums.size() - 1);
     }
 };
