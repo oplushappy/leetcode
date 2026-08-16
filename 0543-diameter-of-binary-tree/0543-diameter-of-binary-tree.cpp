b7af74a9b549670197dd4d83103718555b311219
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-16 13:57:41 +0800

    Time: 0 ms (100%), Space: 23.8 MB (17.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -15,11 +15,10 @@ public:
         int maximum = 0;
         auto f = [&](auto &self, TreeNode* node) {
             if(!node) return 0;
-            int length = 1;
             int left = self(self, node->left);
             int right = self(self, node->right);
             maximum = max(maximum, left + right);
-            return length + max(left, right);
+            return 1 + max(left, right);
         };
         f(f, root);
         return maximum;
