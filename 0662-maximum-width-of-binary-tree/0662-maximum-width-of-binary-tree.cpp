25e81e426d5037d0d3360556b246ceb324c2cdeb
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-17 15:00:40 +0800

    Time: 0 ms (100%), Space: 19.8 MB (45.24%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,44 +12,40 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        vector<vector<int>> m;
-        auto f = [&](auto &self, TreeNode* node, int layer, unsigned long long idx) {
-            if(!node) return;
-            if(m.size() == layer) m.resize(m.size() + 1);
-            m[layer].push_back(idx);
-            self(self, node->left, layer + 1, idx * 2 + 1);
-            self(self, node->right, layer + 1, idx * 2 + 2);
-        };
-        f(f, root, 0, 0);
-        int maximum = 0;
-        for(int i = 0; i < m.size(); i++) {
-            int size = m[i].size();
-            int width = size == 1 ? 1 : m[i][size - 1] - m[i][0] + 1;
-            maximum = max(maximum, width);
+        // vector<vector<int>> m;
+        // auto f = [&](auto &self, TreeNode* node, int layer, unsigned long long idx) {
+        //     if(!node) return;
+        //     if(m.size() == layer) m.resize(m.size() + 1);
+        //     m[layer].push_back(idx);
+        //     self(self, node->left, layer + 1, idx * 2 + 1);
+        //     self(self, node->right, layer + 1, idx * 2 + 2);
+        // };
+        // f(f, root, 0, 0);
+        // int maximum = 0;
+        // for(int i = 0; i < m.size(); i++) {
+        //     int size = m[i].size();
+        //     int width = size == 1 ? 1 : m[i][size - 1] - m[i][0] + 1;
+        //     maximum = max(maximum, width);
+        // }
+        // return maximum;
+        unsigned long long maximum = 0;
+        queue<pair<TreeNode*, unsigned long long>> q;
+        q.push({root, 0});
+        while(!q.empty()) {
+            int size = q.size();
+            unsigned long long base = q.front().second;
+            unsigned long long lastIdx = 0;
+
+            for(int i = 0; i < size; i++) {
+                auto [cur, idx] = q.front();
+                q.pop();
+                idx -= base;
+                lastIdx = idx;
+                if(cur->left) q.push({cur->left, idx * 2 + 1});
+                if(cur->right) q.push({cur->right, idx * 2 + 2});
+            }
+            maximum = max(maximum, lastIdx + 1);
         }
         return maximum;
-                // int maximum = 0;
-        // auto bfs = [&](TreeNode* node) {
-        //     deque<TreeNode*> q;
-        //     q.push_back(node);
-        //     while(!q.empty()) {
-        //         // if(!q.front() && !q.back()) break;
-        //         while(!q.front()) q.pop_front();
-        //         while(!q.back()) q.pop_back();
-                
-        //         int size = q.size();
-        //         int layer = 0;
-                
-        //         for(int i = 0; i < size; i++) {
-        //             TreeNode* cur = q.front();
-        //             q.pop_front();
-        //             if(cur->left) q.push_back(cur->left);
-        //             if(cur->left) q.push_back(cur->left);
-        //         }
-        //         maximum = max(maximum, layer);
-        //     }
-        // };
-        // bfs(root);
-        // return maximum; 
     }
 };
