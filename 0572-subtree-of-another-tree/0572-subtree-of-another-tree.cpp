e8b005b187afd236f9758a0d8fa303745ead9512
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-18 13:49:30 +0800

    Time: 0 ms (100%), Space: 29 MB (70.94%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -12,26 +12,22 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == nullptr && subRoot == nullptr) return true;
-        if(root && subRoot == nullptr) return true;
-        if(root == nullptr && subRoot) return false;
-
-        if(isSameTree(root, subRoot)) {
-            return true;
-        }
-        
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-    }
-
-    bool isSameTree(TreeNode* s, TreeNode* t) {
-        if(s == nullptr && t == nullptr) {
-            return true;
-        }
-
-        if(s && t && s->val == t->val) {
-            return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
-        }
-
-        return false;
+        auto judge = [](auto &self, TreeNode* p, TreeNode* q) {
+            if(!p && !q) return true;
+            if(!p || !q) return false;
+            if(p->val != q->val) return false;
+            return self(self, p->left, q->left) && self(self, p->right, q->right);
+        };
+        auto traverse = [&](auto &self, TreeNode* node1, TreeNode* node2) {
+            if(!node1) return false;
+            if(node1->val == node2->val) {
+                if(judge(judge, node1, node2)) return true;
+            };
+            bool left = self(self, node1->left, node2);
+            bool right = self(self, node1->right, node2);
+            if(left || right) return true;
+            return false;
+        };
+        return traverse(traverse, root, subRoot);
     }
 };
