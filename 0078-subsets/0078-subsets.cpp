17cd1a42095b8a9851c4e4b1709f8873a7448094
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-20 01:07:01 +0800

    Time: 0 ms (100%), Space: 10 MB (37.77%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,19 +3,17 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> subset;
-        dfs(0, nums, subset, res);
-        return res;
-    }
-    void dfs(int index, const vector<int>& nums, vector<int>& subset, vector<vector<int>>& res) {
-        if(index >= nums.size()) {
+        int n = nums.size();
+        auto backtrack = [&](auto self, int lower) {
+            if(subset.size() > nums.size()) return;
             res.push_back(subset);
-            return;
-        }
-        // include
-        subset.push_back(nums[index]);
-        dfs(index + 1, nums, subset, res);
-        // not include
-        subset.pop_back();
-        dfs(index + 1, nums, subset, res);
+            for(int i = lower; i < n; i++) {
+                subset.push_back(nums[i]);
+                self(self, i + 1);
+                subset.pop_back();
+            }
+        };
+        backtrack(backtrack, 0);
+        return res;
     }
 };
