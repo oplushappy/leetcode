a9bb8028a7dad999ff8c210cdd5473d404241826
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-22 20:54:47 +0800

    Time: 0 ms (100%), Space: 16.4 MB (32.95%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -14,23 +14,19 @@ public:
         if(head == nullptr) return head;
         ListNode* dummy = new ListNode(0, head);
         int len = 0;
-        for(auto cur = dummy->next; cur; cur = cur->next) len++;
-        // if(len == 0) return nullptr;
-        // if(len == 1) return dummy->next;
+        auto T = dummy;
+        for(; T->next; T = T->next) len++;
         int step = k % len;
         if(step == 0) return dummy->next;
         int step2 = len - step;
         
-        auto cur = dummy;
-        while(step2--) cur = cur->next;
-        auto nxt1 = cur->next;
-        auto first = dummy->next;
-        cur->next = nullptr;
-        dummy->next = nxt1;
-        
-        cur = nxt1;
-        while(cur->next) cur = cur->next;
-        cur->next = first;
+        auto K = dummy;
+        while(step2--) K = K->next;
+        auto N = K->next;
+        auto F = dummy->next;
+        K->next = nullptr;
+        dummy->next = N;
+        T->next = F;
 
         return dummy->next;
     }
