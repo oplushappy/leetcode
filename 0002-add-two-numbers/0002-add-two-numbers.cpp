ae1df165924f730012722ab38950609975806369
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-24 23:15:40 +0800

    Time: 0 ms (100%), Space: 77.1 MB (46.35%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,23 +11,50 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode * dummy = new ListNode();
-        ListNode * cur = dummy;
-        int carry = 0;
-
-        while(l1 || l2 || carry) {
-            int val1 = (l1 != nullptr) ? l1->val : 0;
-            int val2 = (l2 != nullptr) ? l2->val : 0;
-            
-            int val = val1 + val2 + carry;
-            carry = val / 10;
-            val %= 10;
-            cur->next = new ListNode(val);
-            
-            cur = cur->next;
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        int car = 0;
+        auto dummy = new ListNode(0, 0);
+        auto t1 = dummy;
+        while(l1 && l2) {
+            int tmp = l1->val + l2->val;
+            if(car) tmp += 1;
+            if(tmp >= 10) {
+                t1->next = new ListNode(tmp - 10, 0);
+                car = 1;
+            } else {
+                t1->next = new ListNode(tmp, 0);
+                car = 0;
+            }
+            t1 = t1->next;
+            l1 = l1->next;
+            l2 = l2->next;
         }
+        while(l1) {
+            int tmp = l1->val;
+            if(car) tmp += 1;
+            if(tmp >= 10) {
+                t1->next = new ListNode(tmp - 10, 0);
+                car = 1;
+            } else {
+                t1->next = new ListNode(tmp, 0);
+                car = 0;
+            }
+            t1 = t1->next;
+            l1 = l1->next;
+        }
+        while(l2) {
+            int tmp = l2->val;
+            if(car) tmp += 1;
+            if(tmp >= 10) {
+                t1->next = new ListNode(tmp - 10, 0);
+                car = 1;
+            } else {
+                t1->next = new ListNode(tmp, 0);
+                car = 0;
+            }
+            t1 = t1->next;
+            l2 = l2->next;
+        }
+        if(car) t1->next = new ListNode(1, 0);
         return dummy->next;
     }
 };
