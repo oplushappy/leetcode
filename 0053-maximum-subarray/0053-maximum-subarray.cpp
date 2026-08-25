9cb7719026ed31f6043bebda201c383b7334ede1
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-08-25 11:21:12 +0800

    Time: 0 ms (100%), Space: 74.6 MB (14.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,11 +4,9 @@ public:
         int n = nums.size();
         auto dp = vector(n, 0);
         dp[0] = nums[0];
-        int maximum = dp[0];
         for(int i = 1; i < n; i++) {
             dp[i] = max(nums[i], dp[i-1] + nums[i]);
-            maximum = max(maximum, dp[i]);
         }
-        return maximum;
+        return *max_element(dp.begin(), dp.end());
     }
 };
