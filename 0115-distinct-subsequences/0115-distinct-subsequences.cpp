44465ee4319beb1fc2d63687e788a3e9d22ba8c6
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-09-05 01:20:06 +0800

    Time: 7 ms (96.21%), Space: 8.5 MB (99.97%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,17 @@
 class Solution {
+    const int inf = INT_MIN / 2;
 public:
     int numDistinct(string s, string t) {
         int m = t.size();
-        vector<uint32_t> dp(m + 1);
+        vector<unsigned int> dp(m+1, 0);
         dp[0] = 1;
         for(auto c : s) {
-            for(int i = m - 1; i >= 0; i--) {
-                if(c == t[i]) {
-                    dp[i + 1] += dp[i];
+            for(int i = m; i > 0; i--) {
+                if(c == t[i - 1]) {
+                    dp[i] += dp[i-1];
                 }
             }
         }
         return dp[m];
-        
     }
 };
