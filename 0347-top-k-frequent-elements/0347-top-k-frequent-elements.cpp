216d00bca4b2c23f5253ce611109c53fb4c735bf
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-09-07 13:49:53 +0800

    Time: 2 ms (58.45%), Space: 20.1 MB (5.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> m;
+        unordered_map<int, int> m;
         for(auto &num : nums) {
             m[num]++;
         }
-        map<int, vector<int>, greater<int>> freq;
+        vector<vector<int>> freq(nums.size() + 1);
         for(auto &[key, val] : m) {
             freq[val].push_back(key);
         }
         vector<int> res;
-        for(auto &[key, val] : freq) {
-            int n = val.size();
-            k -= n;
-            for(auto &v : val) res.push_back(v);
-            if(k == 0) break;
+        for(int i = freq.size() - 1; i >= 0; i--) {
+            for(auto &v : freq[i]) {
+                res.push_back(v);
+            }
+            if(res.size() == k) break;
         }
         return res;
     }
