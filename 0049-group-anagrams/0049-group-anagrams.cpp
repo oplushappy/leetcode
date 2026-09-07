ec4b1e5dafb45296b8c0a00cac2f0dd9c6c2f51a
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-09-07 13:30:52 +0800

    Time: 5 ms (99.39%), Space: 23.9 MB (93.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> resMap;
-        for(const string& str : strs) {
+        unordered_map<string, vector<string>> m;
+        for(auto &str : strs) {
             string tmp = str;
             sort(tmp.begin(), tmp.end());
-            resMap[tmp].push_back(str);
+            m[tmp].push_back(str);
         }
         vector<vector<string>> res;
-        for(auto &[key, val] : resMap) {
+        for(auto &[key, val] : m) {
             res.push_back(move(val));
         }
         return res;
