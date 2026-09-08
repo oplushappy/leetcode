983bff5c3a532cb36d1dc7fb2ad1d8379e05a4d5
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-09-08 22:43:40 +0800

    Time: 3 ms (50.42%), Space: 23.8 MB (42.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<int> rows(9, 0), cols(9, 0), block(9, 0);
-
+        auto row = vector(9, vector(10, 0));
+        auto col = vector(9, vector(10, 0));
+        auto cell = vector(3, vector(3, vector(10, 0)));
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(board[i][j] == '.') continue;
-                int num = board[i][j] - '1';
-                int bit = 1 << num;
-                int b = (i / 3) * 3 + (j / 3);
-                if((rows[i] & bit) || (cols[j] & bit) || (block[b] & bit)) {
-                    return false;
-                }
-                rows[i] |= bit;
-                cols[j] |= bit;
-                block[b] |= bit;
-
+                int num = board[i][j] - '0';
+                if(row[i][num]) return false;
+                if(col[j][num]) return false;
+                if(cell[i/3][j/3][num]) return false;
+                row[i][num]++;
+                col[j][num]++;
+                cell[i/3][j/3][num]++;
             }
         }
         return true;
