af97de82720eab4060db6801a4d15ea6c71005d5
oplushappy <91440425+oplushappy@users.noreply.github.com>
2026-09-14 21:10:45 +0800

    Time: 0 ms (100%), Space: 9.7 MB (93.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string tmp = "";
-        for(auto c : s) {
-            if(isalnum(c)) tmp += tolower(c);
-        }
-        int l = 0, r = tmp.size() - 1;
+        int n = s.size();
+        int l = 0, r = n - 1;
         while(l < r) {
-            if(tmp[l] != tmp[r]) return false;
+            while(l < r && !isalnum(s[l])) l++;
+            while(l < r && !isalnum(s[r])) r--;
+            if(tolower(s[l]) != tolower(s[r])) return false;
             l++, r--;
         }
         return true;
